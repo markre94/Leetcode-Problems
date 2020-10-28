b84a6ad1f9335cad060b99ec71327c790432f709
marcin94 <markre94@icloud.com>
2020-10-28 19:47:58 +0100

    sec solution:

--- algoexpert/threeNoSums.py
+++ algoexpert/threeNoSums.py
@@ -2,7 +2,7 @@
 O(n^3) time."""
 
 
-def threeNumberSum(array, targetSum):
+def threeNumberSum1(array, targetSum):
     # Write your code here.
     array.sort()
     result = []
@@ -12,6 +12,35 @@ def threeNumberSum(array, targetSum):
                 if abs(array[i] + array[j] + array[z]) == targetSum:
                     result.append([array[i], array[j], array[z]])
     return result if result else []
+
+
+print(threeNumberSum1([12, 3, 1, 2, -6, 5, -8, 6], 0))
+
+"""effective solution"""
+
+
+def threeNumberSum(array, targetSum):
+    # Write your code here.
+    array.sort()
+    result_arr = []
+    # left = 1
+    # right = len
+    for i in range(len(array)-1):
+        left = i + 1
+        right = len(array) - 1
+
+        while left < right:
+            temp_sum = array[i] + array[left] + array[right]
+            if temp_sum > targetSum:
+                right -= 1
+            elif temp_sum < targetSum:
+                left += 1
+            elif temp_sum == targetSum:
+                result_arr.append([array[i], array[left], array[right]])
+                left += 1
+                right -= 1
+
+    return result_arr if result_arr else []
 
 
 print(threeNumberSum([12, 3, 1, 2, -6, 5, -8, 6], 0))
