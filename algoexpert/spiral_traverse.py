25f27803186f1fd9d261aa9ae7ca7e8de7d00e78
marcin94 <markre94@icloud.com>
2020-11-01 22:48:35 +0100

    works fine

--- algoexpert/spiral_traverse.py
+++ algoexpert/spiral_traverse.py
@@ -1,31 +1,31 @@
 def spiralTraverse(array):
     result = []
     start_row = 0
-    end_row = len(array)
+    end_row = len(array) - 1
     start_col = 0
-    end_col = len(array[0])
+    end_col = len(array[0]) - 1
 
-    while start_row <= end_row - 1 and start_col <= end_col - 1:
+    while start_row <= end_row and start_col <= end_col:
         # first row
-        for i in range(start_col, end_col):
+        for i in range(start_col, end_col+1):
             result.append(array[start_row][i])
 
         # last column
 
-        for i in range(start_row + 1, end_row):
-            result.append(array[i][end_col - 1])
+        for i in range(start_row + 1, end_row+1):
+            result.append(array[i][end_col])
 
         # last row
 
-        for i in range(end_col - 2, start_col - 1, -1):
-            if start_col == end_col:
+        for i in range(end_col - 1, start_col-1, -1):
+            if start_row == end_row:
                 break
-            result.append(array[end_row - 1][i])
+            result.append(array[end_row][i])
 
         # first column
 
-        for i in range(end_row - 2, start_row, -1):
-            if start_row == end_row:
+        for i in range(end_row-1, start_row, -1):
+            if start_col == end_col:
                 break
             result.append(array[i][start_col])
 
