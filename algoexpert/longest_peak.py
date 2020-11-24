090e52ba4e5a947c114777086411da6e7eb47192
marcin94 <markre94@icloud.com>
2020-11-24 23:14:34 +0100

    works

--- algoexpert/longest_peak.py
+++ algoexpert/longest_peak.py
@@ -2,25 +2,23 @@ def longestPeak(array):
     peak_max = 0
     i = 1
     while i < len(array) - 1:
-        peak_val = 0
         if not check_if_is_peak(array[i-1], array[i], array[i+1]):
             i += 1
             continue
-
+        peak_val = 1
         peak_idx = i
         left = peak_idx - 1
         right = peak_idx + 1
 
-        while array[peak_idx] > array[left] and left >= 0:
+        while array[left + 1] > array[left] and left >= 0:
             peak_val += 1
-            peak_idx -= 1
             left -= 1
 
-        peak_idx = i
-        while array[right] < array[peak_idx] and right < len(array) - 1:
+        while array[right - 1] > array[right] and right < len(array):
             peak_val += 1
-            peak_idx += 1
             right += 1
+            if right >= len(array):
+                break
 
         if peak_max <= peak_val:
             peak_max = peak_val
@@ -33,5 +31,5 @@ def check_if_is_peak(left, mid, right):
     return left < mid and mid > right
 
 
-# print(longestPeak(array=[1, 2, 3, 3, 4, 0, 10, 6, 5, -1, -3, 2, 3]))
+print(longestPeak(array=[1, 2, 3, 3, 4, 0, 10, 6, 5, -1, -3, 2, 3]))
 print(longestPeak(array=[1, 2, 3, 0]))
