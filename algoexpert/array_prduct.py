f01d95df329f89e5a7bf074e7c18a70fbec14563
marcin94 <markre94@icloud.com>
2020-11-25 20:57:05 +0100

    Works sweet

--- algoexpert/array_prduct.py
+++ algoexpert/array_prduct.py
@@ -1,11 +1,20 @@
 def arrayOfProducts(array):
     # Write your code here.
+    prod_array = []
     for i in range(len(array)):
-        product = 1
-        for j in range(i + 1, len(array)):
-            product *= array[j]
-        array[i] = product
-    return array
+        left = i - 1
+        right = i + 1
+        product_l = 1
+        product_r = 1
+        while left >= 0:
+            product_l *= array[left]
+            left -= 1
+        while right < len(array):
+            product_r *= array[right]
+            right += 1
+        prod_array.append(product_r * product_l)
+
+    return prod_array
 
 
 print(arrayOfProducts([5, 1, 4, 2]))
