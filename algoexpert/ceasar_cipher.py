c836ba6e2f4a23a6865a2c28786e79120fd3ba41
marcin94 <markre94@icloud.com>
2020-12-03 22:02:47 +0100

    Solved ceaser cipher probelm

--- algoexpert/ceasar_cipher.py
+++ algoexpert/ceasar_cipher.py
@@ -2,14 +2,14 @@ import string as st
 
 
 def caesarCipherEncryptor(string, key):
-    # Write your code here.
+
     result_str = ""
     alphabet = [chr(i) for i in range(ord('a'), ord('z') + 1)]
 
     for i in range(len(string)):
         idx = alphabet.index(string[i]) + key
         if idx > 25:
-            new_idx = idx - 26
+            new_idx = idx % 26
             result_str += alphabet[new_idx]
         else:
             result_str += alphabet[idx]
